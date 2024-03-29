7a4ba40276f33112bc699449c83d8d7edb0b5a19
Ana Hanna <anathehanna@Anas-MacBook-Pro.local>
2024-03-29 00:10:03 -0400

    adding images to about

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -1,29 +1,42 @@
+import { useState } from 'react';
 require('./about.css');
 
-function About() {
-    return <div 
-      className={`resume-container resume-link`} >
-      <h1>Hi, I'm Ana!</h1>
+const ME = 'me';
+const CHRIS = 'chris';
+const PHOEBE = 'phoebe';
+const BIT = 'bit';
+const NIBS = 'nibs';
 
-      <h3>Personal</h3>
-      <p> I studied computer science and mathematics in college which I now use in my job as a software engineer. 
-        At home I have one husband (Chris), one daughter (Phoebe) and two cats (Nibble and Bit). 
-        When I'm not working or taking care of my daughter, you can find me gardening, <a href="/#/travel">
-        traveling </a>, hiking, cooking or playing board games.
-      </p>
-      <h3>Professional</h3>
-      <p>All the important stuff is here: <a href={require('../docs/abealo_9-26-2020_resume.pdf')} target="_blank" rel="noreferrer">
+function About() {
+  const [image, setImage] = useState(ME);
+
+  const meImg = <img src={require("../images/me.jpg")} alt="person smiling"></img>;
+  const chrisImg = <img src={require("../images/chris.jpg")} alt="person smiling"></img>;
+  const phoebeImg = <img src={require("../images/phoebe.jpg")} alt="person smiling"></img>;
+  const bitImg = <img src={require("../images/bit.jpg")} alt="orange cat with boa"></img>;
+  const nibsImg = <img src={require("../images/nibs.jpg")} alt="grey cat on counter"></img>;
+
+  return <div className={`resume-container resume-link`} >
+    <div style={{ display: 'flex', gap: '50px', alignItems: 'center' }}>
+      <div style={{ width: '50%'}}>
+        <h1 style={{ cursor: 'pointer'}} onClick={() => setImage(ME)}>Hi, I'm Ana!</h1>
+        <h3>Personal</h3>
+        <p> I studied computer science and mathematics in college which I now use in my job as a software engineer.
+          At home I have <span style={{ cursor: 'pointer'}} onClick={() => setImage(CHRIS)}>one husband (Chris)</span>, <span style={{ cursor: 'pointer'}} onClick={() => setImage(PHOEBE)}>one daughter (Phoebe)</span> and two cats (<span style={{ cursor: 'pointer'}} onClick={() => setImage(NIBS)}>Nibble</span> and <span style={{ cursor: 'pointer'}} onClick={() => setImage(BIT)}>Bit</span>).
+          When I'm not working or taking care of my daughter, you can find me gardening, <a href="/#/travel">
+            traveling </a>, hiking, cooking or playing board games.
+        </p>
+        <h3>Professional</h3>
+        <p>All the important stuff is here: <a href={require('../docs/abealo_9-26-2020_resume.pdf')} target="_blank" rel="noreferrer">
           View Resume
         </a>
-      </p>
-      <p>Summary:</p>
-      <ul>
-        <li>College: Graduated from the University of Rochester in 2015 with a B.A in matehmatics and a B.S. in Computer Science</li>
-        <li>First job: Factset Inc. A fin tech company headquartered in Norwalk CT. I worked in C++ writing monte carlo simulation algorithms to help predict portfolio outcomes</li>
-        <li>Latest Job: Cimpress. A parent company to custom printing businesses around the world (like Vistaprint). I wear many hats - team lead, engineering manager, and software engineer. Less frequently: UX designer and product manager</li>
-      </ul>
+        </p>
+      </div>
+      <div class="crop">
+        {image === ME ? meImg : image === CHRIS ? chrisImg : image === PHOEBE ? phoebeImg : image === BIT ? bitImg : nibsImg}
+      </div>
     </div>
-  }
-  
-  export default About;
-  
+  </div>
+}
+
+export default About;
